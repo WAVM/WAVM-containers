95d9868a1b1eb1d40622348f1ffe189f385996a7
Andrew Scheidecker <andrew@scheidecker.net>
2019-09-03 17:27:50 -0400

    Add clang-format-check image

--- builders/build.sh
+++ builders/build.sh
@@ -4,3 +4,5 @@ set -e
 
 docker build -t wavm/ubuntu16.04-builder ubuntu16.04
 docker build -t wavm/centos7.0-builder   centos7.0
+
+docker build -t wavm/clang-format-check  clang-format-check
--- builders/push.sh
+++ builders/push.sh
@@ -4,3 +4,5 @@ set -e
 
 docker push wavm/ubuntu16.04-builder
 docker push wavm/centos7.0-builder
+
+docker push wavm/clang-format-check
